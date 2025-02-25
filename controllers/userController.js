964c65ff166b63b03a853ca3852062c306e4330c
anamikatri <159463913+anamikatri@users.noreply.github.com>
2025-02-25 13:34:43 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -99,6 +99,8 @@ export const logout= catchAsyncErrors(async (req, res, next)=>{
   res.status(200).cookie("token", "", {
     expires: new Date(Date.now()),
     httpOnly: true,
+    secure:true,
+    samesite:"None"
   }).json({
     success: true,
     message: "Logout Successfully.",
